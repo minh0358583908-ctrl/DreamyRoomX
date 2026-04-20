381e721497b632149fa757f2ccfba62cadddcbe6
buiducminh <minh0358583908@gmail.com>
2026-04-20 22:36:24 +0700

    bt

--- Assets/_BT1/Scripts/UserActionController.cs
+++ Assets/_BT1/Scripts/UserActionController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class UserActionController : MonoBehaviour
 {
     private GameObject _curObjSelected;
+    private float _oldPosZOfObjSelected;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,13 +35,15 @@ public class UserActionController : MonoBehaviour
     private void HandlePickItem()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //_curObjSelected.transform.position = new Vector3(mousePos.x , mousePos.y , -3f);
         var rayCastPos = new Vector3(mousePos.x, mousePos.y, 0);
-        var rayCastHits = Physics2D.RaycastAll(rayCastPos, Vector2.zero, Mathf.Infinity, LayerMask.GetMask("Default"));
+        var rayCastHits = Physics2D.RaycastAll(rayCastPos, Vector2.zero, Mathf.Infinity, LayerMask.GetMask("GamePlay"));
         foreach (var hit in rayCastHits)
         {
-            if (hit.collider != null)
+            if (hit.collider != null && hit.collider.tag.Equals("Item"))
             {
                 _curObjSelected = hit.collider.gameObject;
+                _oldPosZOfObjSelected = _curObjSelected.transform.position.z;
                 break;
             }
         }
@@ -47,12 +51,15 @@ public class UserActionController : MonoBehaviour
 
     private void HandlePutItem()
     {
+        if (_curObjSelected == null) return;
+        var curPos = _curObjSelected.transform.position;
+        _curObjSelected.transform.position = new Vector3(curPos.x, curPos.y, _oldPosZOfObjSelected);
         _curObjSelected = null;
     }
 
     private void HandleMoveItem()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        _curObjSelected.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        _curObjSelected.transform.position = new Vector3(mousePos.x, mousePos.y, -3);
     }
 }
